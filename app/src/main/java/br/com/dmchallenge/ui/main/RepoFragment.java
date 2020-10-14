3b4de3df62af662989b378a7cd28ee5933e75f9e
vapstor <acaua.pitta@hotmail.com>
2020-10-13 23:47:07 -0300

    view binding enabled

--- app/src/main/java/br/com/dmchallenge/ui/main/RepoFragment.java
+++ app/src/main/java/br/com/dmchallenge/ui/main/RepoFragment.java
@@ -10,13 +10,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import br.com.dmchallenge.R;
+import br.com.dmchallenge.databinding.RepoFragmentBinding;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class RepoFragment extends Fragment {
-
     private RepoViewModel mViewModel;
+    private RepoFragmentBinding binding;
 
     public static RepoFragment newInstance() {
         return new RepoFragment();
@@ -25,13 +25,21 @@ public class RepoFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        binding = RepoFragmentBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(RepoViewModel.class);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
